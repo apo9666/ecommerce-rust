50d4ff2dc5bf37d64078eb6dca50f14b292db855
apo9666 <66564120+apo9666@users.noreply.github.com>
2022-11-05 14:53:02 -0300

    get customer

--- src/api/customer/controller.rs
+++ src/api/customer/controller.rs
@@ -1,21 +1,53 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{
+    get,
+    web::{self, Path},
+    HttpResponse,
+};
 use aws_sdk_dynamodb::{model::AttributeValue, Client};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
-#[get("/customers")]
-pub async fn get_customers(client: web::Data<Client>) -> impl Responder {
+#[derive(Serialize, Deserialize)]
+struct Customer {
+    id: String,
+    email: String,
+    name: String,
+}
+
+pub fn get_s(item: &HashMap<String, AttributeValue>, key: String) -> Option<String> {
+    if let Some(attribute) = item.get(&key) {
+        if let Ok(value) = attribute.as_s() {
+            return Some(value.clone());
+        }
+    }
+
+    None
+}
+
+#[get("/customers/{id}")]
+pub async fn get_customers(
+    client: web::Data<Client>,
+    path: Path<String>,
+) -> Result<HttpResponse, Box<dyn std::error::Error>> {
+    let id = format!("c#{}", path.into_inner());
     let result = client
         .get_item()
-        .key("pk", AttributeValue::S("teste".into()))
-        .key("sk", AttributeValue::S("teste".into()))
+        .key("pk", AttributeValue::S(id.clone()))
+        .key("sk", AttributeValue::S(id.clone()))
         .table_name("teste")
         .send()
-        .await
-        .expect("get item");
+        .await?;
 
     if let Some(item) = result.item {
-        let response = item.get("pk").unwrap().as_s().unwrap();
-        HttpResponse::Ok().body(response.clone())
+        let response = Customer {
+            id: get_s(&item, "pk".into()).unwrap(),
+            email: get_s(&item, "email".into()).unwrap(),
+            name: get_s(&item, "name".into()).unwrap(),
+        };
+
+        let response = serde_json::to_string(&response).unwrap();
+        Ok(HttpResponse::Ok().body(response))
     } else {
-        HttpResponse::Ok().body("not ok")
+        Ok(HttpResponse::NotFound().body("not found"))
     }
 }
