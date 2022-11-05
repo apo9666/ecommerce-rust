e6482da17b291d794b169c6ea25eb819b4a5bdd7
apo9666 <66564120+apo9666@users.noreply.github.com>
2022-11-05 09:50:56 -0300

    add env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod api;
 mod routes;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 use aws_sdk_dynamodb as dynamodb;
 use routes::{customer, health_check};
 
@@ -10,8 +10,11 @@ async fn main() -> std::io::Result<()> {
     let config = aws_config::load_from_env().await;
     let client = dynamodb::Client::new(&config);
 
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
     HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .app_data(web::Data::new(client.clone()))
             .configure(health_check::route)
             .configure(customer::route)
