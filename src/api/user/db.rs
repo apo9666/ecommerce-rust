b633ee742e984b31d55d21299fbd32904423c9d1
apo9666 <66564120+apo9666@users.noreply.github.com>
2023-01-28 15:46:31 -0300

    add get_user_by_email

--- src/api/user/db.rs
+++ src/api/user/db.rs
@@ -6,9 +6,12 @@ use aws_sdk_dynamodb::{
 use bcrypt::{hash, verify, DEFAULT_COST};
 use chrono::{SecondsFormat, Utc};
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
+use uuid::{uuid, Uuid};
 
-use crate::error::{AppError, AppErrorType};
+use crate::{
+    error::{AppError, AppErrorType},
+    lib::dynamodb::get_s,
+};
 
 #[derive(Serialize, Deserialize)]
 pub struct User {
@@ -71,6 +74,7 @@ pub async fn create_user(client: &Client, new_user: &NewUser) -> Result<User, Ap
                         .table_name(USER_TABLE)
                         .item("pk", AttributeValue::S(user_email.clone()))
                         .item("sk", AttributeValue::S(user_email.clone()))
+                        .item("user_id", AttributeValue::S(id.clone()))
                         .condition_expression("attribute_not_exists(pk)")
                         .build(),
                 )
@@ -85,4 +89,59 @@ pub async fn create_user(client: &Client, new_user: &NewUser) -> Result<User, Ap
         })?;
 
     Ok(user)
+}
+
+pub async fn get_user_by_email(client: &Client, email: &str) -> Result<Option<User>, AppError> {
+    let user_email = format!("{}{}", "UE#", email.clone());
+    let result = client
+        .get_item()
+        .table_name(String::from(USER_TABLE))
+        .key("pk", AttributeValue::S(user_email.clone()))
+        .key("sk", AttributeValue::S(user_email.clone()))
+        .send()
+        .await
+        .map_err(|error| AppError {
+            cause: Some(error.to_string()),
+            message: None,
+            error_type: AppErrorType::DbError,
+        })?;
+
+    let Some(item) = result.item else {
+        return Ok(None);
+    };
+
+    let id = get_s(&item, "user_id")?;
+
+    let result = client
+        .get_item()
+        .table_name(String::from(USER_TABLE))
+        .key("pk", AttributeValue::S(id.clone()))
+        .key("sk", AttributeValue::S(id.clone()))
+        .send()
+        .await
+        .map_err(|error| AppError {
+            cause: Some(error.to_string()),
+            message: None,
+            error_type: AppErrorType::DbError,
+        })?;
+
+    match result.item {
+        None => Ok(None),
+        Some(item) => {
+            let id = get_s(&item, "pk")?;
+            let id = &id[2..id.len()];
+
+            Ok(Some(User {
+                id: Uuid::parse_str(id).map_err(|error| AppError {
+                    cause: Some(error.to_string()),
+                    message: None,
+                    error_type: AppErrorType::DbError,
+                })?,
+                email: get_s(&item, "email")?,
+                name: get_s(&item, "name")?,
+                created_at: get_s(&item, "created_at")?,
+                updated_at: get_s(&item, "updated_at")?,
+            }))
+        }
+    }
 }
